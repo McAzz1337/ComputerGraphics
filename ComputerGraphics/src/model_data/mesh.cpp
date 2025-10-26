27c9e147d40b48d2fbde17b129fd205f22b20277
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2025-10-26 19:30:28 +0100

    changed constructors to no longer reqire a texture path and now bind requires the projection view matrix and passes it to the material

--- ComputerGraphics/src/model_data/mesh.cpp
+++ ComputerGraphics/src/model_data/mesh.cpp
@@ -1,20 +1,16 @@
 #include "mesh.h"
 
-Mesh::Mesh() {
-
-}
-
-Mesh::Mesh(std::string texturePath) : tex(Texture(texturePath)) {
-
-}
+Mesh::Mesh() {}
 
 Mesh::Mesh(VertexArray vao, IndexBuffer ibo) : vao(vao), ibo(ibo) {}
 
-Mesh::Mesh(VertexArray vao, IndexBuffer ibo, std::string texturePath) : vao(vao), ibo(ibo), tex(Texture(texturePath)) {}
 
 Mesh::Mesh(VertexBuffer vbo, IndexBuffer ibo) : vao(VertexArray(vbo)) {}
 
-Mesh::Mesh(VertexBuffer vbo, IndexBuffer ibo, std::string texturePath) : vao(VertexArray(vbo)), tex(Texture(texturePath)) {}
+Mesh::Mesh(const Mesh& other): vao(other.vao), ibo(other.ibo), mat(other.mat) {
+
+	}
+
 
 Mesh::~Mesh() {}
 
@@ -28,29 +24,15 @@ void Mesh::assignBuffers(VertexBuffer vbo, IndexBuffer ibo) {
 	this->ibo = ibo;
 }
 
-void Mesh::assignTexture(const std::string& path) {
-	tex.load(path);
-}
-
-void Mesh::assignTexture1(const std::string& path) {
-	tex1.load(path);
-}
-
-void Mesh::assignBumpMap(const std::string& path) {
-	bump.load(path);
-}
 
 void Mesh::assignMaterial(Material mat) {
 	this->mat = mat;
 }
 
-void Mesh::bind(Shader* shader) const {
+void Mesh::bind(const glm::mat4& projectionView) const {
 	vao.bind();
 	ibo.bind();
-	tex.bind();
-	tex1.bind(1);
-	//bump.bind(1);
-	//mat.bind(shader);
+	mat.bind(projectionView);
 }
 
 VertexBuffer& Mesh::getVbo() {
--- ComputerGraphics/src/model_data/mesh.h
+++ ComputerGraphics/src/model_data/mesh.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "vertexArray.h"
-#include "../texture.h"
+#include "texture.h"
 #include  "../shader.h"
 #include "material.h"
 
@@ -8,29 +8,21 @@
 class Mesh {
 	VertexArray vao;
 	IndexBuffer ibo;
-	Texture tex;
-	Texture bump;
-	Texture tex1;
 	Material mat;
 
 
 public:
 	Mesh();
-	Mesh(std::string texturePath);
 	Mesh(VertexArray vao, IndexBuffer ibo);
-	Mesh(VertexArray vao, IndexBuffer ibo, std::string texturePath);
 	Mesh(VertexBuffer vbo, IndexBuffer ibo);
-	Mesh(VertexBuffer vbo, IndexBuffer ibo, std::string texturePath);
+	Mesh(const Mesh& other);
 	~Mesh();
 
 	void assignVertexArray(VertexArray vao, IndexBuffer ibo);
 	void assignBuffers(VertexBuffer vbo, IndexBuffer ibo);
-	void assignTexture(const std::string& path);
-	void assignTexture1(const std::string& path);
-	void assignBumpMap(const std::string& path);
 	void assignMaterial(Material mat);
 
-	void bind(Shader* shader) const;
+	void bind(const glm::mat4& projectionView) const;
 
 	VertexBuffer& getVbo();
 	IndexBuffer& getIbo();
