d205b7990bf6af8cd368f1ba10d5c8b7c42765f1
McAzz1337 <99556184+McAzz1337@users.noreply.github.com>
2025-11-16 01:23:03 +0100

    Adjusted function signature to accept a vector of GeneralMesh*

--- ComputerGraphics/src/creation.h
+++ ComputerGraphics/src/creation.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <vector>
 #include "model_data/mesh.h"
+#include "model_data/generalMash.h"
 #include "io/assetImporter.h"
 
 
-void createMeshes(std::vector<Mesh>& meshes);
+void createMeshes(std::vector<GeneralMesh*>& meshes);
